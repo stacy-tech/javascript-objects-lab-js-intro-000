616409cfdb927fb54835fe933db8d29652d7b9be
Stacy Daniel <stacydn23@gmail.com>
2020-09-20 00:56:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var receipe = new Object();
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({},object,{[key]:value})
 }
-function destructivelyUpdateObjectWithKeyAndValuve(object,key,value){
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
   object [key] = value;
   return object;
 }
