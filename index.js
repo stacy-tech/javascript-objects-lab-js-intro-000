888409b090963319540aea75b3b0976f2537ed08
Stacy Daniel <stacydn23@gmail.com>
2020-09-20 00:53:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({},object,{[key]:value})
 }
 function destructivelyUpdateObjectWithKeyAndValuve(object,key,value){
-  object [key] = value
-  return object
+  object [key] = value;
+  return object;
+}
+function deleteFromObjectByKey(object,key){
+  var clone = Object.assign ({},object);
+  delete clone[key];
+  return clone
+}
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key];
+  return object;
 }
